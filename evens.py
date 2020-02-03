28b035877559a742c36593af82ffd4352883d48e
Evan L <evan.lindsey1@marist.edu>
2020-02-03 17:28:57 -0500

    Solution to evens loop exercise.

---
+++ evens.py
@@ -0,0 +1,22 @@
+Python 3.7.4 (tags/v3.7.4:e09359112e, Jul  8 2019, 20:34:20) [MSC v.1916 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> for i in range(1,10,3):
+	print (i)
+
+1
+4
+7
+>>> for i in range (2,20,2):
+	print(i)
+
+	
+2
+4
+6
+8
+10
+12
+14
+16
+18
+>>> 
