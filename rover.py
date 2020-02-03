0ceb8ba98eb7822acde051e8d220a0e414286117
Evan L <evan.lindsey1@marist.edu>
2020-02-03 17:23:26 -0500

    Solution to Curiosity rover exercise.

---
+++ rover.py
@@ -0,0 +1,8 @@
+Python 3.7.4 (tags/v3.7.4:e09359112e, Jul  8 2019, 20:34:20) [MSC v.1916 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> my var = 34000000 / 186000
+SyntaxError: invalid syntax
+>>> 
+ RESTART: C:\Users\maristuser\AppData\Local\Programs\Python\Python37\rover.py 
+182.79569892473117
+>>> 
