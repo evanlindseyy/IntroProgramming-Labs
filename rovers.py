c7c131eb53569aa87b327a0e231439f750d75fc3
Evan L <evan.lindsey1@marist.edu>
2020-02-03 22:55:44 -0500

    Solution to Curiosity rover exercise.

---
+++ rovers.py
@@ -0,0 +1,11 @@
+def main():
+    print ("The Mars Curiosity rover takes photos of the Mars surface and then transmits them to NASA at the speed of light")
+main()
+print()
+def main ():
+    print ("At it's closest orbit to Earth, Mars is 34 million miles away. With the speed of light being 186,000 miles per second, how long will it take for a photo from the Curiosity to reach NASA?")
+main()
+print()
+myvar = str(34000000 / 186000)
+print()
+print("It will take " + myvar + " seconds to reach Curiosity")
