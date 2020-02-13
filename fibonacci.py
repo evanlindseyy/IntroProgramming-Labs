62ce169780bad7261681c30c4e2418dbe3f96040
Evan L <evan.lindsey1@marist.edu>
2020-02-12 19:21:43 -0500

    Wrote an interactive Fibonacci calculator.

---
+++ fibonacci.py
@@ -0,0 +1,11 @@
+n= int(input("Please enter the Fibonacci number you would like to see: "))
+
+def fib(n):
+    
+    a = ((1+5**.5)/2)
+
+    b = ((1-5**.5)/2)
+    
+    return ((a**n-(b)**n/5**.5))
+
+print (int((fib(n))))
