718232e9394a78b024e7688c2e34c9d736a7bc4c
Evan L <evan.lindsey1@marist.edu>
2020-02-17 17:16:04 -0500

    Usability improvements to the guessing game.

--- guessing-game.py
+++ guessing-game.py
@@ -1,25 +1,27 @@
+#Variables & Functions
 def title():
     print("Python Guessing Game!")
+    print("Type 'quit' at any point to exit.")
 
 def end():
     print("Congrats! You guessed correctly.")
 
-
 a = "elephant"
 
-
-
-
-
-
+#Script
 title()
     
 while True:
-    answer = input("I am thinking of an animal...: ")
+    answer = input("I am thinking of an animal...: ").lower()
     if answer == a:
+        end()
+        break
+        end()
+    elif answer == "quit":
+        print("Thanks for playing!")
         break
     else:
         print("That's not it... try again!")
         continue
 
-end()
+
