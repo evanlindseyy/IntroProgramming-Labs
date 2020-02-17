fb5f226c903b12d743d2c808c735693c752c4336
Evan L <evan.lindsey1@marist.edu>
2020-02-17 17:29:55 -0500

    More usability improvements to the guessing game.

--- guessing-game.py
+++ guessing-game.py
@@ -1,10 +1,13 @@
 #Variables & Functions
 def title():
     print("Python Guessing Game!")
-    print("Type 'quit' at any point to exit.")
+    print("Type any word starting with 'q' to quit.")
 
 def end():
     print("Congrats! You guessed correctly.")
+
+def thanks():
+    print("Thanks for Playing!")
 
 a = "elephant"
 
@@ -15,10 +18,17 @@ while True:
     answer = input("I am thinking of an animal...: ").lower()
     if answer == a:
         end()
-        break
-        end()
-    elif answer == "quit":
-        print("Thanks for playing!")
+        answer = input("Do you like elephants? Enter yes (y) or no (n): ").lower()
+        if answer.startswith("y"):
+            print("I do too! That's why I picked them for this game.")
+            thanks()
+            break
+        if answer.startswith("n"):
+            print("I'm sorry to hear that...")
+            thanks()
+            break
+    elif answer.startswith("q"):
+        print("You quit the game. Thanks for playing!")
         break
     else:
         print("That's not it... try again!")
