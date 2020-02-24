5351689c2ac39b1e8247304256d0a1c0e3aa539b
Evan L <evan.lindsey1@marist.edu>
2020-02-24 17:55:12 -0500

    A modular design.

--- credentials.py
+++ credentials.py
@@ -2,18 +2,43 @@
 # Lab #5 – Working with Strings and Functions
 # Author: Evan Lindsey
 # Created: 2020-02-24
-def main():
-# get user's first and last names
-    first = input("Enter your first name: ")
-    last = input("Enter your last name: ")
-# TODO modify this to generate a Marist-style username
-    uname = first + "." + last + "1@marist.edu"
-# ask user to create a new password
+fn = "Enter your first name: "
+ln = "Enter your last name: "
+
+
+
+def Name(f, l):
+    global first
+    global last
+    first = input(f)
+    last = input(l)
+    print()
+    print(first, last)
+    print()
+    return first, last
+
+def Marist():
+    global uname
+    uname = first + "." + last + "1"
+    print("Your username is", uname)
+    print()
+    
+
+def Password():
     passwd = input("Create a new password: ")
-    if len(passwd) < 8:
-        print("Fool of a Took! That password is feeble")
-        passwd = input("Create a new password: ")
-    elif len(passwd) >= 8:
-        print("The force is strong in this one…")
-        print("Account configured. Your new email address is", uname)
-main()
+    while True:
+        if len(passwd) < 8:
+            print("Fool of a Took! That password is feeble")
+            passwd = input("Create a new password: ")
+            continue
+        elif len(passwd) >= 8:
+            print("The force is strong in this one…")
+            print("Account configured. Your new email address is", uname + "@marist.edu")
+            break
+first,last = Name(fn, ln)
+Marist()
+Password()
+
+
+
+
